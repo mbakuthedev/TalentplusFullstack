6d7295637043c3507c75ab48f9a8cd7609e427cb
Adeogo Favour <Favourdamilareadeogo@gmail.com>
2022-11-17 01:31:18 +0100

    fixed directory issues

---
+++ TalentplusApi/TalentplusApi/Controllers/MessageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TalentplusApi.Services;
+
+namespace TalentplusApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private  Message _message;
+        public MessageController(Message message)
+        {
+            _message = message;
+        }
+        [HttpGet]
+        public IActionResult Greet()
+        {
+            return Ok(_message.SayHi());   
+        }
+    }
+}
---
+++ TalentplusApi/TalentplusApi/Services/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TalentplusApi.Models;
+
+namespace TalentplusApi.Services
+{
+    public class Message
+    {
+        public MessageModel SayHi()
+        {
+            MessageModel message = new MessageModel();
+            message.Message = "Hi TalentPlus";
+            return message;
+        }
+    }
+}
